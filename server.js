1c747187f11f79c1124641e2d7882e3ce905a30e
Nessa BestA <nessacor16@gmail.com>
2020-02-13 18:10:56 -0500

    upDate

---
+++ server.js
@@ -0,0 +1,38 @@
+// Obtain packages and PORT.
+
+var express = require("express");
+var path = require('path');
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// express.json and express.urlEncoded make it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+require("./app/routing/apiRoutes")(app, path);
+require("./app/routing/htmlRoutes")(app, path);
+
+
+
+// Static files
+app.use(express.static('app/public'));
+
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
